0ccd7c0e68f7390797750b3b80b3946c556942fe
vpikalova <vpikalova@hotmail.com>
2016-04-16 21:36:32 +0300

    KZ 5.2 second commit

--- courses/admin.py
+++ courses/admin.py
@@ -4,12 +4,12 @@ from courses.models import Course, Lesson
 
 class LessonInline(admin.TabularInline):
     model = Lesson
-    extra = 0
+    extra = 2
 
 class CourseAdmin(admin.ModelAdmin):    
-    inlines = [LessonInline]
     list_display = ('name', 'short_description')
     search_fields = ['name']
+    inlines = [LessonInline]
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -5,15 +5,13 @@ from students.models import Student
 
 
 class StudentAdmin(admin.ModelAdmin):
+	list_display = ('Full_name','email', 'skype')
 	search_fields = ['surname', 'email']
-	list_display = ['Full_name', 'email', 'skype']
-	list_filter = ['courses']
+	filter_horizontal = ['courses']
 	fieldsets = [
         ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
         ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
         (None, {'fields': ['courses']})
-]
-
-
+    ] 
 admin.site.register(Student, StudentAdmin)
 
