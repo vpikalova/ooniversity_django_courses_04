50616ce1a850489ee991639c028f7c067c67fb5a
vpikalova <vpikalova@hotmail.com>
2016-05-14 16:09:33 +0300

    KZ 11.2 version1

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,115 @@
-from django.test import TestCase
 
-# Create your tests here.
+
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
+from courses.models import Course, Lesson
+from coaches.models import Coach
+
+
+def course_create(ccount=1):
+	for i in xrange(ccount):	
+		course = Course.objects.create(
+				name = 'Course {}'.format(i+1),
+				short_description = 'Description {}'.format(i+1),
+				)
+	return course
+
+
+class CoursesListTest(TestCase):
+	'''
+	Test index page
+	'''
+
+	def test_course_list(self):		
+		client = Client()
+		response = client.get('/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_new_course_count(self):
+		course_create(10)	
+		self.assertEqual(Course.objects.all().count(), 10)
+
+	def test_link_add_course(self):
+		response = self.client.get('/')
+		self.assertContains(response, '/courses/add/')	
+
+	def test_link_edit_all_course(self):
+		course_create(5)
+		response = self.client.get('/')	
+		for i in range(1, 5):
+			self.assertContains(response, '/courses/edit/{}/'.format(i))
+
+	def test_link_delete_all_course(self):
+		course_create(5)
+		response = self.client.get('/')	
+		for i in range(1, 5):
+			self.assertContains(response, '/courses/remove/{}/'.format(i))			
+
+
+class CoursesDetailTest(TestCase):
+    '''
+    Test for view course detail page
+    '''
+
+    def test_course_view(self):
+    	course = course_create()
+        client = Client()
+        response = client.get(reverse('courses:detail', 
+        								args=(1,)))
+        self.assertEqual(response.status_code, 200)
+
+    def test_lesson_theme(self):
+        course = course_create()
+        for i in xrange(10):
+            newlesson = Lesson.objects.create(
+                subject=u'Theme {}'.format(i+1),
+                description='Test description {}'.format(i+1),
+                course=course,
+                order=i+1,)
+
+        client = Client()
+        response = client.get(reverse('courses:detail',
+            args=(1,)))
+        self.assertContains(response, newlesson.subject)
+
+    def test_course_lesson_order(self):
+        '''
+        Test for exists lesson_name on course page
+        '''
+        course = course_create()
+        for i in xrange(10):
+            newlesson = Lesson.objects.create(
+                subject=u'Theme {}'.format(i+1),
+                description='Test description {}'.format(i+1),
+                course=course,
+                order=i+1,)
+
+        client = Client()
+        response = client.get(reverse('courses:detail', 
+                                        args=(1,)))
+        self.assertContains(response, newlesson.order)
+
+    def test_course_lesson_description(self):
+        course = course_create()
+        for i in xrange(10):
+            newlesson = Lesson.objects.create(
+                subject=u'Theme {}'.format(i+1),
+                description='Test description {}'.format(i+1),
+                course=course,
+                order=i+1,)
+
+        client = Client()
+        response = client.get(reverse('courses:detail', 
+                                        args=(1,)))
+        self.assertContains(response, newlesson.description)
+
+    def test_course_name(self):
+		course = course_create()
+		client = Client()
+		response = client.get(reverse('courses:detail', 
+										args=(1,)))
+		self.assertContains(response, course.name)
+
+Status API Training Shop Blog About
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,97 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
+from students.models import Student
 
-# Create your tests here.
+
+def student_create(stcount=1):
+	for i in xrange(stcount):	
+		student = Student.objects.create(
+				name = 'Name{}'.format(i+1),
+				surname = 'Surname{}'.format(i+1),
+				date_of_birth = '2016-05-05',
+				email = 'mail@mail.com'
+				)
+	return student	
+
+class StudentsListTest(TestCase):
+	'''
+	Test student list page
+	'''
+
+	def test_student_list(self):
+		client = Client()
+		response = client.get('/students/')
+		self.assertEqual(response.status_code, 200)
+
+	def test_student_count(self):
+		student_create(10)
+		self.assertEqual(Student.objects.all().count(), 10)	
+
+	def test_student_surname(self):
+		student = student_create()
+		client = Client()
+		response = client.get('/students/')
+		self.assertContains(response, student.surname)
+
+	def test_student_name(self):
+		student = student_create()
+		client = Client()
+		response = client.get('/students/')
+		self.assertContains(response, student.name)
+
+	def test_link_add_student(self):
+		response = self.client.get('/students/')
+		self.assertContains(response, '/students/add/')	
+
+	def test_link_edit_student(self):
+		student = student_create()
+		response = self.client.get('/students/')	
+		self.assertContains(response, '/students/edit/1/')
+
+	def test_link_delete_student(self):
+		student = student_create()
+		response = self.client.get('/students/')	
+		self.assertContains(response, '/students/remove/1/')
+
+
+class StudentsDetailTest(TestCase):
+	'''
+	Test student detail page
+	'''
+
+	def test_student_view(self):
+		student = student_create()
+		client = Client()
+		response = client.get(reverse('students:detail', 
+										args=(1,)))
+		self.assertEqual(response.status_code, 200)
+
+	def test_student_mail(self):
+		student = student_create()
+		client = Client()
+		response = client.get(reverse('students:detail', 
+										args=(1,)))
+		self.assertContains(response, student.email)
+
+	def test_student_phone(self):
+		student = student_create()
+		client = Client()
+		response = client.get(reverse('students:detail', 
+										args=(1,)))
+		self.assertContains(response, student.phone)
+
+	def test_student_address(self):
+		student = student_create()
+		client = Client()
+		response = client.get(reverse('students:detail', 
+									args=(1,)))
+		self.assertContains(response, student.address)
+
+	def test_student_skype(self):
+		student = student_create()
+		client = Client()
+		response = client.get(reverse('students:detail', 
+										args=(1,)))
+		self.assertContains(response, student.skype)
